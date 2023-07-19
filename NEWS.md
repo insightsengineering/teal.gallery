bf2deaa45ac00e4e6477ae2745e0866de4d46af4
Vedha Viyash <49812166+vedhav@users.noreply.github.com>
2023-07-19 18:41:13 +0530

    Refactor teal.gallery into a directory structure for individual shiny apps (#31)
    Closes #29 and #20
    
    ---------
    
    Co-authored-by: github-actions <41898282+github-actions[bot]@users.noreply.github.com>

--- NEWS.md
+++
@@ -1,3 +0,0 @@
-# teal.gallery 0.1.0.9011
-
-* Initial release of `teal.gallery`.
--- R/teal.gallery.R
+++
@@ -1,5 +0,0 @@
-#' `teal.gallery` Package
-#'
-#' `teal.gallery` is a gallery of sample apps based on the Teal framework.
-#' @keywords internal
-"_PACKAGE"
