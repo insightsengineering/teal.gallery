f0287039013884fca94dec0dbc0223eec83f341b
Vedha Viyash <49812166+vedhav@users.noreply.github.com>
2023-08-30 14:35:28 +0530

    Make all the demo apps consistent with same footer and front page (#58)
    #56 - Make all the demo apps consistent with same footer and front page

--- python/app.R
+++ python/app.R
@@ -99,7 +99,38 @@ app <- teal::init(
     )
   ),
   header = "Example teal app using python connector",
-  footer = ""
+  footer = tags$p(
+    actionLink("showAboutModal", "About,"),
+    tags$a(
+      href = "https://github.com/insightsengineering/teal.gallery/tree/main/python",
+      target = "_blank",
+      "Source Code,"
+    ),
+    tags$a(
+      href = "https://github.com/insightsengineering/teal.gallery/issues",
+      target = "_blank",
+      "Report Issues"
+    )
+  )
+)
+
+body(app$server)[[length(body(app$server)) + 1]] <- quote(
+  observeEvent(input$showAboutModal, {
+    showModal(modalDialog(
+      tags$p("This teal app is brought to you by the NEST Team at Roche/Genentech. For more information, please visit:"),
+      tags$ul(
+        tags$li(tags$a(
+          href = "https://github.com/insightsengineering", "Insights Engineering",
+          target = "blank"
+        )),
+        tags$li(tags$a(
+          href = "https://pharmaverse.org", "Pharmaverse",
+          target = "blank"
+        ))
+      ),
+      easyClose = TRUE
+    ))
+  })
 )
 
 shinyApp(app$ui, app$server)
