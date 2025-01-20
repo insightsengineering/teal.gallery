21f73ec223008ccdd7c30460b9b866eb0398a652
Xyarz <lars-andersen@gmx.de>
2025-01-20 14:28:35 +0100

    Initial bsafe setup

--- _internal/utils/generate_app_readme.R
+++ _internal/utils/generate_app_readme.R
@@ -4,11 +4,16 @@ library(yaml)
 app_names <- unlist(lapply(yaml::read_yaml("_internal/quarto/demo-apps.yml"), function(x) x$apps))
 
 for (app_name in app_names) {
+  if(app_name == "bsafe") {
+    graphic = FALSE
+  } else {
+    graphic = TRUE
+  }
   rmarkdown::render(
     "_internal/utils/app_readme_template.Rmd",
     output_dir = app_name,
     output_file = "README.md",
     knit_root_dir = paste0("../../", app_name),
-    params = list(app_name = app_name)
+    params = list(app_name = app_name, graphic = graphic)
   )
 }
