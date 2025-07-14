8a57a215097d3b1f7d3bc7d2dea43421814d2aa6
vedhav <vedhaviyash4@gmail.com>
2025-07-14 08:48:13 +0200

    fix: update cypress test to work on the new navigation

--- longitudinal/tests/cypress/e2e/app.cy.js
+++ longitudinal/tests/cypress/e2e/app.cy.js
@@ -1,50 +1,35 @@
-describe('app', () => {
+describe("app", () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.visit("/");
   });
 
-  it('Starts', () => {});
+  it("Starts", () => {});
 
-  it('Has 8 tabs', () => {
-    cy
-      .get('.nav.nav-pills a[data-bs-toggle=tab]', { timeout: 30000 })
-      .should('have.length', 8);
+  it("Has 8 tabs", () => {
+    cy.get("ul.teal-modules-tree a.module-button", { timeout: 30000 }).should(
+      "have.length",
+      8
+    );
   });
 
-  it('Navigates to all tabs without error', () => {
-    cy
-      .get('.nav.nav-pills a[data-bs-toggle=tab]', { timeout: 30000 })
-      .each(($el) => {
-        cy.wrap($el).as('tealTab');
+  it("Navigates to all tabs without error", () => {
+    cy.get("ul.teal-modules-tree a.module-button", {
+      timeout: 30000,
+    }).then(($moduleButtons) => {
+      cy.wrap($moduleButtons).each(($el) => {
+        cy.wrap($el).as("tealTab");
 
-        cy.get('@tealTab').then(($el2) => {
+        cy.get("@tealTab").then(($el2) => {
           cy.log(`Navigating to: ${$el2[0].innerText}`);
         });
 
-        cy.get('@tealTab').click();
-
-        cy.get('@tealTab').invoke('attr', 'href').as('hrefTab');
-
-        // Make sure that html element does not have a class that indicates
-        // that shiny is busy
-        cy.get('html').not('.shiny-busy');
-
-        cy
-          .get('@hrefTab')
-          .then((hrefTab) => {
-            cy
-              .get(`${hrefTab}.tab-pane.active`)
-              .should('be.visible')
-              .within(() => {
-                cy
-                  .get('*')
-                  .filter(':visible')
-                  .should('have.length.gte', 1)
-                  .then(($el3) => {
-                    cy.wrap($el3).contains(/.+/);
-                  });
-              });
-          });
+        cy.get(".dropdown.nav-item-custom").trigger("mouseover");
+        cy.get(".dropdown-menu").should("have.class", "show");
+        cy.get("@tealTab").click();
+        cy.get("@tealTab").invoke("attr", "href").as("hrefTab");
+        cy.get(".dropdown.nav-item-custom").trigger("mouseout");
+        cy.get("html").not(".shiny-busy");
       });
+    });
   });
 });
