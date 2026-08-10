f05628c091c09a05508544cbf58c8d2ea3ca1b94
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-08-10 13:19:04 +0100

    Fix problems with workflows (#288)
    It fixes:
    - failure to install chrome -- updating container image
    - failure to install dependencies -- bad state of renv.lock files
    - failure to pass tests -- apps taking too long to load due to
    random.cdisc.data being generated
    
    It improves:
    - deploy.yml runs in preview mode (doesn't deploy nor commit to branch)
    - deploy.yml runs stable and dev deployments seperately

--- exploratory/app.R
+++ exploratory/app.R
@@ -7,7 +7,6 @@ options(
 ## Data reproducible code ----
 data <- teal_data()
 data <- within(data, {
-  library(random.cdisc.data)
   library(dplyr)
   library(tidyr)
   library(ggExtra)
@@ -26,10 +25,9 @@ data <- within(data, {
   library(colourpicker)
   library(sparkline)
 
-
-  ADSL <- radsl(seed = 1)
-  ADRS <- radrs(ADSL, seed = 1)
-  ADLB <- radlb(ADSL, seed = 1)
+  ADSL <- random.cdisc.data::cadsl
+  ADRS <- random.cdisc.data::cadrs
+  ADLB <- random.cdisc.data::cadlb
   ADLBPCA <- ADLB %>%
     dplyr::select(USUBJID, STUDYID, SEX, ARMCD, AVAL, AVISIT, PARAMCD) %>%
     tidyr::pivot_wider(
