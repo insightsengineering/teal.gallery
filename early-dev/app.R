c84fd73023a36c66d972cdc82ba6a468828f358a
André Veríssimo <211358+averissimo@users.noreply.github.com>
2026-08-14 00:10:12 +0100

    feat: support osprey

--- early-dev/app.R
+++ early-dev/app.R
@@ -163,46 +163,6 @@ aeflag_vars <- c("RELFL", "CTC35FL", "SERFL", "RELSERFL")
 facet_vars <- c("SEX", "BMRKR2", "RACE", "STRATA1", "STRATA2")
 ds_vars <- c("EOSSTT", "DCSREAS")
 
-cs_arm_var <- choices_selected(
-  choices = variable_choices(ADSL, subset = arm_vars),
-  selected = "ARM"
-)
-
-cs_aeflag_var <- choices_selected(
-  choices = variable_choices(ADAE, subset = aeflag_vars),
-  selected = NULL
-)
-
-cs_aeterm_var <- choices_selected(
-  choices = variable_choices(ADAE, subset = c("AEDECOD", "AETERM", "AELLT", "AESOC", "AEBODSYS", "AEHLT")),
-  selected = "AEDECOD"
-)
-
-cs_facet_var <- choices_selected(
-  choices = variable_choices(ADSL, subset = facet_vars),
-  selected = NULL
-)
-
-cs_ds_var <- choices_selected(
-  choices = variable_choices(ADSL, ds_vars),
-  selected = ds_vars
-)
-
-cs_bar_var <- choices_selected(
-  choices = variable_choices(ADSL, c("TRTDURD", "EOSDY")),
-  selected = "TRTDURD"
-)
-
-cs_paramcd_rsp <- choices_selected(
-  choices = value_choices(ADRS, "PARAMCD", "PARAM", subset = c("BESRSPI", "INVET")),
-  selected = "BESRSPI"
-)
-
-cs_paramcd_tr <- choices_selected(
-  choices = value_choices(ADTR, "PARAMCD", "PARAM", subset = "SLDINV"),
-  selected = "SLDINV"
-)
-
 picks_paramcd_rsp <- picks(
   variables("PARAMCD", "PARAMCD"),
   values(c("BESRSPI", "INVET"), "BESRSPI", multiple = FALSE),
@@ -265,54 +225,37 @@ app <- init(
       tm_g_events_term_id(
         label = "Common AE Plot",
         dataname = "ADAE",
-        term_var = cs_aeterm_var,
-        arm_var = cs_arm_var,
+        parentname = "ADSL",
+        term_var = teal.picks::variables(c("AEDECOD", "AETERM", "AELLT", "AESOC", "AEBODSYS", "AEHLT"), "AEDECOD"),
+        arm_var = teal.picks::variables(arm_vars, "ARM"),
         plot_height = c(800, 200, 2000)
       ),
       tm_g_ae_oview(
         label = "AE Overview Plot",
         dataname = "ADAE",
-        arm_var = cs_arm_var,
-        flag_var_anl = choices_selected(
-          choices = variable_choices(ADAE, c("AEREL1", "AEREL2")),
-          selected = "AEREL1"
-        ),
+        arm_var = teal.picks::variables(arm_vars, "ARM"),
+        flag_var_anl = teal.picks::variables(c("AEREL1", "AEREL2"), "AEREL1"),
         plot_height = c(800, 200, 2000)
       ),
       tm_g_butterfly(
         label = "Butterfly Plot",
         dataname = "ADAE",
-        filter_var = cs_aeflag_var,
-        right_var = cs_arm_var,
-        left_var = cs_arm_var,
-        category_var = choices_selected(
-          choices = variable_choices(ADAE, subset = c("AEDECOD", "AETERM", "AESOC", "AEBODSYS")),
-          selected = "AEBODSYS"
-        ),
-        color_by_var = choices_selected(
-          choices = c(variable_choices(ADAE, "AETOXGR"), "None"),
-          selected = "AETOXGR"
-        ),
-        count_by_var = choices_selected(
-          choices = c("# of patients", "# of AEs"),
-          selected = "# of patients"
-        ),
-        facet_var = cs_facet_var,
-        sort_by_var = choices_selected(choices = c("count", "alphabetical"), selected = "count"),
+        filter_var = teal.picks::variables(aeflag_vars, NULL),
+        right_var = teal.picks::variables(arm_vars, "ARM"),
+        left_var = teal.picks::variables(arm_vars, "ARM"),
+        category_var = teal.picks::variables(c("AEDECOD", "AETERM", "AESOC", "AEBODSYS"), "AEBODSYS"),
+        color_by_var = teal.picks::variables(c("AETOXGR", "None"), "AETOXGR"),
+        count_by_var = teal.picks::values(c("# of patients", "# of AEs"), "# of patients"),
+        facet_var = teal.picks::variables(facet_vars, NULL),
+        sort_by_var = teal.picks::values(c("count", "alphabetical"), "count"),
         legend_on = TRUE,
         plot_height = c(600, 200, 2000)
       ),
       tm_g_ae_sub(
         label = "AE by Subgroup",
         dataname = "ADAE",
-        arm_var = choices_selected(
-          selected = "ACTARMCD",
-          choices = c("ACTARM", "ACTARMCD")
-        ),
-        group_var = choices_selected(
-          selected = c("SEX", "REGION1", "RACE"),
-          choices = c("SEX", "REGION1", "RACE")
-        ),
+        arm_var = teal.picks::variables(c("ACTARM", "ACTARMCD"), "ACTARMCD"),
+        group_var = teal.picks::variables(c("SEX", "REGION1", "RACE"), c("SEX", "REGION1", "RACE"), multiple = TRUE),
         plot_height = c(600, 200, 2000)
       )
     ),
@@ -326,19 +269,13 @@ app <- init(
     tm_g_spiderplot(
       label = "Spider plot",
       dataname = "ADTR",
-      paramcd = cs_paramcd_tr,
-      x_var = choices_selected(choices = variable_choices(ADTR, "ADY"), selected = "ADY"),
-      y_var = choices_selected(
-        choices = variable_choices(ADTR, c("PCHG", "CHG", "AVAL")),
-        selected = "PCHG"
-      ),
-      marker_var = choices_selected(
-        choices = variable_choices(ADSL, c("SEX", "RACE")),
-        selected = "SEX"
-      ),
-      line_colorby_var = cs_arm_var,
-      xfacet_var = cs_facet_var,
-      yfacet_var = cs_facet_var,
+      paramcd = picks_paramcd_tr,
+      x_var = teal.picks::variables("ADY", "ADY"),
+      y_var = teal.picks::variables(c("PCHG", "CHG", "AVAL"), "PCHG"),
+      marker_var = teal.picks::variables(c("SEX", "RACE"), "SEX"),
+      line_colorby_var = teal.picks::variables(arm_vars, "ARM"),
+      xfacet_var = teal.picks::variables(facet_vars, NULL),
+      yfacet_var = teal.picks::variables(facet_vars, NULL),
       vref_line = "42",
       href_line = "-20, 0, 30",
       anno_txt_var = TRUE,
@@ -348,50 +285,33 @@ app <- init(
     tm_g_swimlane(
       label = "Swimlane Plot",
       dataname = "ADRSSWIM",
-      bar_var = cs_bar_var,
-      bar_color_var = choices_selected(
-        choices = variable_choices(ADSL, fact_vars_asl),
-        selected = "EOSSTT"
-      ),
-      sort_var = choices_selected(
-        choices = variable_choices(ADSL, c(arm_vars, "TRTDURD")),
-        selected = arm_vars[1]
-      ),
-      marker_pos_var = choices_selected(
-        choices = c(variable_choices(ADRS, "ADY")),
-        selected = "ADY"
-      ),
-      marker_shape_var = choices_selected(c(variable_choices(ADRS, c("AVALC", "AVISIT"))),
-        selected = "AVALC"
-      ),
+      bar_var = teal.picks::variables(c("TRTDURD", "EOSDY"), "TRTDURD"),
+      bar_color_var = teal.picks::variables(is.factor, "EOSSTT"),
+      sort_var = teal.picks::variables(c(arm_vars, "TRTDURD"), 1L),
+      marker_pos_var = teal.picks::variables("ADY", "ADY"),
+      marker_shape_var = teal.picks::variables(c("AVALC", "AVISIT"), "AVALC"),
       marker_shape_opt = c("CR" = 16, "PR" = 17, "SD" = 18, "PD" = 15, "Death" = 8),
-      marker_color_var = choices_selected(
-        choices = c(variable_choices(ADRS, c("AVALC", "AVISIT"))),
-        selected = "AVALC"
-      ),
+      marker_color_var = teal.picks::variables(c("AVALC", "AVISIT"), "AVALC"),
       marker_color_opt = c(
         "CR" = "green", "PR" = "blue", "SD" = "goldenrod",
         "PD" = "red", "Death" = "black"
       ),
       vref_line = c(30, 60),
-      anno_txt_var = choices_selected(
-        choices = variable_choices(ADSL, intersect(arm_vars, fact_vars_asl)),
-        selected = arm_vars[1]
-      )
+      anno_txt_var = teal.picks::variables(all_of(arm_vars) & where(is.factor), 1L)
     ),
     tm_g_waterfall(
       label = "Waterfall Plot",
       dataname_tr = "ADTRWF",
       dataname_rs = "ADRS",
-      bar_paramcd = cs_paramcd_tr,
-      bar_var = choices_selected(variable_choices(ADTRWF, "PCHG"), "PCHG"),
-      bar_color_var = choices_selected(variable_choices(ADSL, fact_vars_asl), "ARMCD"),
-      sort_var = choices_selected(variable_choices(ADSL, fact_vars_asl), NULL),
-      add_label_var_sl = choices_selected(variable_choices(ADSL, fact_vars_asl), NULL),
-      add_label_paramcd_rs = cs_paramcd_rsp,
-      anno_txt_var_sl = choices_selected(variable_choices(ADSL, fact_vars_asl), NULL),
-      anno_txt_paramcd_rs = cs_paramcd_rsp,
-      facet_var = cs_facet_var,
+      bar_paramcd = picks_paramcd_tr,
+      bar_var = teal.picks::variables("PCHG", "PCHG"),
+      bar_color_var = teal.picks::variables(is.factor, "ARMCD"),
+      sort_var = teal.picks::variables(is.factor, NULL),
+      add_label_var_sl = teal.picks::variables(is.factor, NULL),
+      add_label_paramcd_rs = picks_paramcd_rsp,
+      anno_txt_var_sl = teal.picks::variables(is.factor, NULL),
+      anno_txt_paramcd_rs = picks_paramcd_rsp,
+      facet_var = teal.picks::variables(facet_vars, NULL),
       ytick_at = 20,
       href_line = "-30, 20",
       gap_point_val = NULL,
@@ -399,45 +319,21 @@ app <- init(
     ),
     tm_g_patient_profile(
       label = "Patient Profile Plot",
-      patient_id = choices_selected(
-        choices = unique(ADSL$USUBJID),
-        selected = unique(ADSL$USUBJID)[1]
-      ),
+      patient_id = teal.picks::variables("USUBJID", "USUBJID"),
       sl_dataname = "ADSL",
       ex_dataname = "ADEX",
       ae_dataname = "ADAE",
       rs_dataname = "ADRS",
       cm_dataname = "ADCM",
       lb_dataname = "ADLB",
-      sl_start_date = choices_selected(
-        selected = "TRTSDTM",
-        choices = variable_choices(ADSL, subset = c("TRTSDTM", "RANDDT"))
-      ),
-      ex_var = choices_selected(
-        selected = "PARCAT2",
-        choices = variable_choices(ADEX, "PARCAT2")
-      ),
-      ae_var = choices_selected(
-        selected = "AEDECOD",
-        choices = variable_choices(ADAE, c("AEDECOD", "AESOC"))
-      ),
-      ae_line_col_var = choices_selected(
-        selected = "AESER",
-        choices = variable_choices(ADAE, c("AESER", "AEREL"))
-      ),
+      sl_start_date = teal.picks::variables(c("TRTSDTM", "RANDDT"), "TRTSDTM"),
+      ex_var = teal.picks::variables("PARCAT2", "PARCAT2"),
+      ae_var = teal.picks::variables(c("AEDECOD", "AESOC"), "AEDECOD"),
+      ae_line_col_var = teal.picks::variables(c("AESER", "AEREL"), "AESER"),
       ae_line_col_opt = c("Y" = "red", "N" = "blue"),
-      rs_var = choices_selected(
-        selected = "PARAMCD",
-        choices = variable_choices(ADRS, c("PARAMCD", "PARAM"))
-      ),
-      cm_var = choices_selected(
-        selected = "CMDECOD",
-        choices = variable_choices(ADCM, c("CMDECOD", "CMCAT"))
-      ),
-      lb_var = choices_selected(
-        selected = "PARAMCD",
-        choices = variable_choices(ADLB, c("PARAMCD", "PARAM"))
-      ),
+      rs_var = teal.picks::variables(c("PARAMCD", "PARAM"), "PARAMCD"),
+      cm_var = teal.picks::variables(c("CMDECOD", "CMCAT"), "CMDECOD"),
+      lb_var = teal.picks::variables(c("PARAMCD", "PARAM"), "PARAMCD"),
       x_limit = "-28, 750",
       plot_height = c(1200, 400, 5000)
     )
